1f92a806a76ef7b4a6f621c3b47b4234890e1f7d
GitHub Action <actions@github.com>
2022-05-05 16:17:07 +0000

    Add Go models

---
+++ go/models/com_sarrubia_eventbus_avro_example_embedded/EventMetadata.go
@@ -0,0 +1,6 @@
+package com_sarrubia_eventbus_avro_example_embedded
+
+// EventMetadata
+type EventMetadata struct {
+	EventMetadataBase
+}
---
+++ go/models/com_sarrubia_eventbus_avro_example_embedded/user_login_attemp_embedded.go
@@ -0,0 +1,6 @@
+package com_sarrubia_eventbus_avro_example_embedded
+
+// UserLoginAttemptEvent example app' schema
+type UserLoginAttemptEvent struct {
+	UserLoginAttemptEventBase
+}
