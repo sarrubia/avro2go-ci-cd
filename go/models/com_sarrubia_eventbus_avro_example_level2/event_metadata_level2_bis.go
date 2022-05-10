ef74ecb3e2eed7813384a1e908e34aef59b8fc66
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-05-10 13:22:13 +0000

    Adding Go models from Avro schema by bot

---
+++ go/models/com_sarrubia_eventbus_avro_example_level2/event_metadata_level2_bis.go
@@ -0,0 +1,6 @@
+package com_sarrubia_eventbus_avro_example_level2
+
+// EventMetadataLevel2Bis
+type EventMetadataLevel2Bis struct {
+	EventMetadataLevel2BisBase
+}
---
+++ go/models/com_sarrubia_eventbus_avro_example_level2/event_metadata_level2_bis1.go
@@ -0,0 +1,6 @@
+package com_sarrubia_eventbus_avro_example_level2
+
+// EventMetadataLevel2Bis1
+type EventMetadataLevel2Bis1 struct {
+	EventMetadataLevel2Bis1Base
+}
