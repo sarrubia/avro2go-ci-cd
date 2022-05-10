d6f4a33d82809de3716fcd6ebb1b712b4e12f9ec
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-05-10 11:44:11 +0000

    Adding Go models from Avro schema by bot

--- go/models/com_sarrubia_eventbus_avro_example/user_login_attemp_event_base.go
+++ go/models/com_sarrubia_eventbus_avro_example/user_login_attemp_event_base.go
@@ -3,18 +3,18 @@
 package com_sarrubia_eventbus_avro_example
 
 // UserLoginAttemptEventAVSC Avro schema to send to Schema Registry
-const UserLoginAttemptEventAVSC = "{\"type\":\"record\",\"name\":\"UserLoginAttemptEvent\",\"namespace\":\"com.sarrubia.eventbus.avro.example\",\"doc\":\"example app' schema\",\"fields\":[{\"name\":\"email\",\"type\":{\"avro.java.string\":\"String\",\"type\":\"string\"},\"default\":\"NONE\"},{\"name\":\"ipMachina\",\"type\":{\"avro.java.string\":\"String\",\"type\":\"string\"},\"default\":\"NONE\"},{\"name\":\"date\",\"type\":\"long\",\"default\":0},{\"name\":\"result\",\"type\":{\"avro.java.string\":\"String\",\"type\":\"string\"},\"default\":\"NONE\"},{\"name\":\"metas\",\"type\":{\"items\":{\"type\":\"com.sarrubia.eventbus.avro.example.EventMetadata\"},\"type\":\"array\"},\"default\":null}]}"
+const UserLoginAttemptEventAVSC = "{\"type\":\"record\",\"name\":\"UserLoginAttemptEvent\",\"namespace\":\"com.sarrubia.eventbus.avro.example\",\"doc\":\"example app' schema\",\"fields\":[{\"name\":\"email\",\"type\":{\"avro.java.string\":\"String\",\"type\":\"string\"},\"default\":\"NONE\"},{\"name\":\"ipMac\",\"type\":{\"avro.java.string\":\"String\",\"type\":\"string\"},\"default\":\"NONE\"},{\"name\":\"date\",\"type\":\"long\",\"default\":0},{\"name\":\"result\",\"type\":{\"avro.java.string\":\"String\",\"type\":\"string\"},\"default\":\"NONE\"},{\"name\":\"metas\",\"type\":{\"items\":{\"type\":\"com.sarrubia.eventbus.avro.example.EventMetadata\"},\"type\":\"array\"},\"default\":null}]}"
 
 // UserLoginAttemptEventSubject schema registry subject
 const UserLoginAttemptEventSubject = "com.sarrubia.eventbus.avro.example.UserLoginAttemptEvent"
 
 // UserLoginAttemptEventBase example app' schema
 type UserLoginAttemptEventBase struct {
-	Email     string          `avro:"email"`
-	IpMachina string          `avro:"ipMachina"`
-	Date      int64           `avro:"date"`
-	Result    string          `avro:"result"`
-	Metas     []EventMetadata `avro:"metas"`
+	Email  string          `avro:"email"`
+	IpMac  string          `avro:"ipMac"`
+	Date   int64           `avro:"date"`
+	Result string          `avro:"result"`
+	Metas  []EventMetadata `avro:"metas"`
 }
 
 // Schema [REQUIRED] returns the schema const that belongs to UserLoginAttemptEvent
